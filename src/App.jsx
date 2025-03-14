674dd5b78f205fd13f6eff9a6da6da76e79a68aa
randikasptra <saputrarandika857@gmail.com>
2025-03-14 14:34:09 +0700

    Menambahkan file login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import Button from './components/Elements/Button/Buttons';
 
 export default function App() {
   return (
     <>
-    <div className="flex justify-center min-h-screen items-center">
-      <div className="w-full max-w-xs">
-        <h1 className="text-3xl font-bold mb-2 text-slate-600">Login</h1>
-        <p className='font-medium text-slate-500'> Welcome, please your details</p>
-        <form action="">
-          <div className="mb6">
-            <label htmlFor="" className="block text-sm font-medium text-slate-600">Email</label>
-          </div>
-        </form>
+      <div className="flex justify-center min-h-screen items-center">
+        <div className="w-full max-w-xs">
+          <h1 className="text-3xl font-bold mb-2 text-slate-600">Login</h1>
+          <p className='font-medium text-slate-500'> Welcome, please your details</p>
+          <form action="">
+            <div className="my-6">
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-slate-600">
+                Email
+              </label>
+              <input 
+              type="text" 
+              className='text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50' placeholder='example@gmail.com'/>
+            </div>
+            <div className="my-6">
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-slate-600">
+                Password
+              </label>
+              <input 
+              type="password" 
+              className='text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50' placeholder='******'/>
+            </div>
+            <Button classname="bg-blue-600 w-full">Login</Button>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   )
 }
