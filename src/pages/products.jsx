9b55ab697c975efd9bd475586c0210aceb5a2ccc
randikasptra <saputrarandika857@gmail.com>
2025-03-24 23:01:29 +0700

    Menambah file Shoes-3

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -8,6 +8,16 @@ const products = () => {
           <img src="/public/shoes-1.jpg" alt="" />
         </a>
       </div>
+      <div className="  w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <a href="">
+          <img src="/public/shoes-2.jpg" alt="" />
+        </a>
+      </div>
+      <div className="  w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <a href="">
+          <img src="/public/shoes-3.jpg" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
