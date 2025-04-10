2dd9df853aec85c8a92537599ffd91beb72af0fc
randikasptra <saputrarandika857@gmail.com>
2025-04-10 17:28:31 +0700

    Menambahkan card products

---
+++ src/components/Fragments/CardProducts.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const CardProducts = () => {
+    return (
+        <div className='flex justify-center'>
+            <div className="  w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                <a href="">
+                    <img src="/public/shoes-1.jpg" alt="" />
+                </a>
+            </div>
+            <div className="  w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                <a href="">
+                    <img src="/public/shoes-2.jpg" alt="" />
+                </a>
+            </div>
+            <div className="  w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                <a href="">
+                    <img src="/public/shoes-3.jpg" alt="" />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default CardProducts
